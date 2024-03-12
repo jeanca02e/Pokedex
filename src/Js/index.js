a49c357846ef60ee4b91468a7a2fef3a739b4c10
jjtana <dexydax@gmail.com>
2024-03-11 22:26:51 -0400

    hola

---
+++ src/Js/index.js
@@ -0,0 +1,30 @@
+const contPokemons = document.getElementById("cont-pokemons");
+const offset = 0;
+
+const getApi = async(limit=50)=>{
+ const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+ const data = await response.json();
+
+ console.log(data);
+
+ const promise = data.results.map(async(pokemon)=>{
+    const res = await fetch(pokemon.url);
+    const data = await res.json();
+    return data;
+ })
+ const results = await Promise.all(promise);
+ console.log(results);
+ for(let i = 0; i < results.length; i++){
+    const a = document.createElement("div");
+    a.className = "pokemons";
+    a.innerHTML = `
+    <div class = "cont-img"><img src="${results[i].sprites.front_default}" /></div>
+    <p>${results[i].name}</p>`;
+    
+    contPokemons.appendChild(a);
+
+ }
+
+}
+
+getApi();
